740bd1b16db3ff5499777c9f6021b482373abe8c
Ray Redondo <rdredondo@lcdev.xyz>
2025-01-29 11:07:05 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import '../app.css';
+    let { children } = $props();
+</script>
+
+<div class="h-screen bg-zinc-900">
+    <div class="flex h-16 flex-row items-center justify-center bg-zinc-800">
+        <h1 class="font-lc text-3xl text-white">Lightning Creations</h1>
+    </div>
+    {@render children()}
+    <div class="flex h-8 items-center justify-center">
+        <p class="text-xs text-white">&copy; 2025 Lightning Creations</p>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div id="bg-image"></div>
+
+<style>
+    @media (min-aspect-ratio: 1/1) {
+        #bg-image {
+            background-image: url("/bg/wide.png");
+            background-position: center;
+            background-size: cover;
+            height: calc(100% - 4rem - 2rem);
+        }
+    }
+
+    @media (max-aspect-ratio: 1/1) {
+        #bg-image {
+            background-image: url("/bg/tall.png");
+            background-position: top;
+            background-size: cover;
+            height: calc(100% - 4rem - 2rem);
+        }
+    }
+</style>
