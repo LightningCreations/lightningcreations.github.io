cc57116861178d94a1f5f5f73182f3cae24684e3
Ray Redondo <rdredondo@lcdev.xyz>
2025-01-30 14:19:03 -0600

    add new body font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@
     let { children } = $props();
 </script>
 
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</svelte:head>
+
 <div class="h-screen flex flex-col">
     <div class="flex h-16 flex-row items-center justify-center bg-muted">
         <Nav />
