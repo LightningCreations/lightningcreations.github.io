1d2b5a4640d316c67c50069d56d4cbc67b67c7e5
Ray Redondo <rdredondo@lcdev.xyz>
2025-01-30 14:22:49 -0600

    remove spaces

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,6 +5,6 @@
 <div class="p-12 container">
     <h1 class="text-6xl">Contact</h1>
     <p>Administration: <Mailto href="admin@lcdev.xyz" /></p>
-    <p>Branding / graphics: <Mailto href="graphics@lcdev.xyz" /></p>
+    <p>Branding/graphics: <Mailto href="graphics@lcdev.xyz" /></p>
     <p>Open source: <Mailto href="opensource@lcdev.xyz" /></p>
 </div>
