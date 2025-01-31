c6642181d1f0dff7e5ce5e0a96cc220f02fd1893
Ray Redondo <rdredondo@lcdev.xyz>
2025-01-30 22:50:18 -0600

    change text size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 <div class="h-screen flex flex-col">
     <div class="flex h-16 flex-row items-center justify-center bg-muted">
         <Nav />
-        <h1 class="text-3xl"><a href="/">Lightning Creations</a></h1>
+        <h1 class="text-2xl"><a href="/">Lightning Creations</a></h1>
     </div>
     <div class="overflow-scroll flex-1">
         {@render children()}
